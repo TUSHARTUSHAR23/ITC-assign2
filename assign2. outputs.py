7aa66fdb61a919619de7589a3effd31d9f723f49
TUSHARTUSHAR23 <107211946+TUSHARTUSHAR23@users.noreply.github.com>
2022-06-09 11:19:03 -0700

    Add files via upload

---
+++ assign2. outputs.py
@@ -0,0 +1,35 @@
+Enter a number to convert it into its binary equivalent:45
+ Its Binary value:
+01011011 ends here
+2 starts here
+Select an operation to perform : 
+1. ADD
+2. SUBTRACT
+3. MULTIPLY
+4. DIVIDE
+1
+Enter first number: 45
+Enter second number: 44
+The sum is :89
+ends with 2
+3rd starts here
+Enter the value of a :56
+Enter the value of b :67
+Enter the value of c :78
+Ans = 9594
+Enter the value of n :6
+Ans = 15.0
+Ans = 4*math.pi*r**2
+Ans = math.sqrt(r*(math.cos(a))*2+r(math.sin(a))**2
+Ans = y2-y1)/(x2-x1
+3 ends here
+0 1 2 3 4 
+3 4 5 6 7 8 9 
+4 7 10 
+15 13 11 9 7 
+7
+This program finds the total molecular weight of a combination of hydrogen, carbon and oxygen atoms :
+How many hydrogen atoms? 5
+Carbon? 6
+Oxygen? 7
+The total molecular weight is 189.09969999999998
